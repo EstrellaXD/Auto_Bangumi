5dab9acf577cd5b2168f554044418014e74de33f
EstrellaXD <estrellaxd05@gmail.com>
2022-07-09 11:55:46 +0800

    修复log无法显示的bug

--- auto_bangumi/conf/log.py
+++ auto_bangumi/conf/log.py
@@ -7,8 +7,6 @@ def setup_logger():
     DATE_FORMAT = "%Y-%m-%d %X"
     LOGGING_FORMAT = "%(asctime)s %(levelname)s: %(message)s"
     logging.basicConfig(
-        filename=settings.log_path,
-        filemode="w",
         level=level,
         datefmt=DATE_FORMAT,
         format=LOGGING_FORMAT,
