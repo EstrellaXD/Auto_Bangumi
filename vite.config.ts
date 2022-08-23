692af7c02170852b976ee88140926355bc086baa
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2022-08-23 19:58:01 +0800

    修改基础路径为相对路径，避免反代到二级目录时的资源访问问题

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     AutoImport({
