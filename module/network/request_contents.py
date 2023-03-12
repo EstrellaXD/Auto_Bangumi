106e4847c0634cda87b5d710eda47fc7881a02ae
WeijiangChen <ourweijiang@gmail.com>
2023-03-12 22:34:47 +0800

    修正类重载忘改函数了

--- module/network/request_contents.py
+++ module/network/request_contents.py
@@ -17,7 +17,7 @@ class TorrentInfo:
 class RequestContent(RequestURL):
     # Mikanani RSS
     def get_torrents(self, _url: str) -> [TorrentInfo]:
-        soup = self._req.get_content(_url)
+        soup = self.session.get_content(_url)
         torrent_titles = [item.title.string for item in soup.find_all("item")]
         torrent_urls = [item.get("url") for item in soup.find_all("enclosure")]
         torrents = []
@@ -27,11 +27,11 @@ class RequestContent(RequestURL):
         return torrents
 
     def get_torrent(self, _url) -> TorrentInfo:
-        soup = self._req.get_content(_url)
+        soup = self.session.get_content(_url)
         item = soup.find("item")
         enclosure = item.find("enclosure")
         return TorrentInfo(item.title.string, enclosure["url"])
 
     # API JSON
     def get_json(self, _url) -> dict:
-        return self._req.get_content(_url, content="json")
+        return self.session.get_content(_url, content="json")
