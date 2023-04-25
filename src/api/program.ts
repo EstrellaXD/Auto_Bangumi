3ee4f6360b7b586a65fd6b0e5eea886ae41b96ef
Rewrite0 <y747556795@gmail.com>
2023-04-25 18:00:24 +0800

    增加启,停,状态api

--- src/api/program.ts
+++ src/api/program.ts
@@ -2,3 +2,15 @@ import axios from 'axios';
 
 /** 重启 */
 export const appRestart = () => axios.get('/api/v1/restart');
+
+/** 启动 */
+export const appStart = () => axios.get('/api/v1/start');
+
+/** 停止 */
+export const appStop = () => axios.get('/api/v1/stop');
+
+/** 状态 */
+export async function appStatus() {
+  const { data } = await axios.get('/api/v1/getStatus');
+  return data;
+}
