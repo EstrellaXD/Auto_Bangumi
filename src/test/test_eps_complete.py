95ae30cbb1d7622e5e9c1c3c078df41c6da4fd9c
EstrellaXD <estrellaxd05@gmail.com>
2023-05-03 11:16:00 +0800

    Update README.md

--- src/test/test_eps_complete.py
+++
@@ -1,9 +0,0 @@
-from module.core.api_func import FullSeasonGet
-from module.models import Config
-
-
-def test_full_season_get():
-    settings = Config()
-    fsg = FullSeasonGet(settings)
-
-    fsg.get_season_torrents()
