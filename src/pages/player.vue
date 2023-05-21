9d1a3754ad6ae2a8ccfc18fb9b2d83e78bd2b476
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2023-05-21 23:09:35 +0800

    player page

---
+++ src/pages/player.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+definePage({
+  name: 'Player',
+});
+</script>
+
+<template>
+  <iframe
+    src="http://192.168.0.100:8096"
+    frameborder="0"
+    allowfullscreen="true"
+    w-full
+    h-full
+    flex-1
+    rounded-12px
+  ></iframe>
+</template>
+
+<style lang="scss" scope></style>
