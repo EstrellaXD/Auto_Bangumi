b093de106170d02d8dcacb31b076bfdad12a8312
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2023-05-28 17:04:44 +0800

    fix type error

--- src/components/ab-add-bangumi.vue
+++ src/components/ab-add-bangumi.vue
@@ -7,7 +7,27 @@ const show = defineModel('show', { default: false });
 
 const rss = ref('');
 const message = useMessage();
-const rule = ref<BangumiRule>();
+const rule = ref<BangumiRule>({
+  added: false,
+  deleted: false,
+  dpi: '',
+  eps_collect: false,
+  filter: [],
+  group_name: '',
+  id: 0,
+  official_title: '',
+  offset: 0,
+  poster_link: '',
+  rss_link: [],
+  rule_name: '',
+  save_path: '',
+  season: 1,
+  season_raw: '',
+  source: null,
+  subtitle: '',
+  title_raw: '',
+  year: null,
+});
 const analysis = reactive({
   loading: false,
   next: false,
