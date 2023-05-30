d0cb009b1e2080c3a93f7f3b544e881f6d2b7dff
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2023-05-30 21:05:08 +0800

    fix: 修正sidebar奇怪的圆角问题

--- src/views/ab-sidebar.vue
+++ src/views/ab-sidebar.vue
@@ -70,75 +70,76 @@ const items = [
   <div
     :class="[show ? 'w-240px' : 'w-72px']"
     bg-theme-col
-    rounded-12px
     text-white
-    overflow-hidden
     transition-width
-    flex="~ col"
     pb-12px
+    rounded-12px
   >
-    <div
-      w-full
-      h-60px
-      is-btn
-      f-cer
-      bg="#E7E7E7"
-      text="#2A1C52"
-      rel
-      @click="toggle"
-    >
-      <div :class="[!show && 'abs opacity-0']" transition-opacity>
-        <div text-h1>Menu</div>
+    <div overflow-hidden wh-full flex="~ col">
+      <div
+        w-full
+        h-60px
+        is-btn
+        f-cer
+        rounded-t-10px
+        bg="#E7E7E7"
+        text="#2A1C52"
+        rel
+        @click="toggle"
+      >
+        <div :class="[!show && 'abs opacity-0']" transition-opacity>
+          <div text-h1>Menu</div>
+        </div>
+
+        <MenuUnfold
+          theme="outline"
+          size="24"
+          fill="#2A1C52"
+          abs
+          left="24px"
+          :class="[show && 'rotate-y-180']"
+        />
       </div>
 
-      <MenuUnfold
-        theme="outline"
-        size="24"
-        fill="#2A1C52"
-        abs
-        left="24px"
-        :class="[show && 'rotate-y-180']"
-      />
-    </div>
+      <RouterLink
+        v-for="i in items"
+        :key="i.id"
+        :to="i.path"
+        replace
+        :title="i.label"
+        fx-cer
+        px-24px
+        space-x-42px
+        h-48px
+        is-btn
+        transition-colors
+        hover:bg="#F1F5FA"
+        hover:text="#2A1C52"
+        :class="[
+          route.path === i.path && 'bg-[#F1F5FA] text-[#2A1C52]',
+          i.hidden && 'hidden',
+        ]"
+      >
+        <Component :is="i.icon" :size="24" />
+        <div text-h2>{{ i.label }}</div>
+      </RouterLink>
 
-    <RouterLink
-      v-for="i in items"
-      :key="i.id"
-      :to="i.path"
-      replace
-      :title="i.label"
-      fx-cer
-      px-24px
-      space-x-42px
-      h-48px
-      is-btn
-      transition-colors
-      hover:bg="#F1F5FA"
-      hover:text="#2A1C52"
-      :class="[
-        route.path === i.path && 'bg-[#F1F5FA] text-[#2A1C52]',
-        i.hidden && 'hidden',
-      ]"
-    >
-      <Component :is="i.icon" :size="24" />
-      <div text-h2>{{ i.label }}</div>
-    </RouterLink>
-
-    <div
-      title="logout"
-      mt-auto
-      fx-cer
-      px-24px
-      space-x-42px
-      h-48px
-      is-btn
-      transition-colors
-      hover:bg="#F1F5FA"
-      hover:text="#2A1C52"
-      @click="logout"
-    >
-      <Logout :size="24" />
-      <div text-h2>Logout</div>
+      <div
+        title="logout"
+        mt-auto
+        fx-cer
+        px-24px
+        space-x-42px
+        h-48px
+        is-btn
+        transition-colors
+        hover:bg="#F1F5FA"
+        hover:text="#2A1C52"
+        @click="logout"
+      >
+        <Logout :size="24" />
+        <div text-h2>Logout</div>
+      </div>
     </div>
   </div>
 </template>
