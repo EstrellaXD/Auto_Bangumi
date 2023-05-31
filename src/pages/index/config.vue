e1a0a5228a0eded74946b2a341471711d449007f
Rewrite0 <49576640+Rewrite0@users.noreply.github.com>
2023-05-31 23:19:25 +0800

    优化 config 页布局

--- src/pages/index/config.vue
+++ src/pages/index/config.vue
@@ -9,22 +9,28 @@ definePage({
 </script>
 
 <template>
-  <div gap-20px grid="~ cols-2">
-    <config-normal></config-normal>
+  <div h-full flex="~ col">
+    <div grid="~ cols-2" gap-20px mb-auto>
+      <div space-y-20px>
+        <config-normal></config-normal>
 
-    <config-parser></config-parser>
+        <config-parser></config-parser>
 
-    <config-download></config-download>
+        <config-download></config-download>
 
-    <config-manage></config-manage>
+        <config-manage></config-manage>
+      </div>
 
-    <config-notification></config-notification>
+      <div space-y-20px>
+        <config-notification></config-notification>
 
-    <config-proxy></config-proxy>
+        <config-proxy></config-proxy>
 
-    <config-player></config-player>
+        <config-player></config-player>
+      </div>
+    </div>
 
-    <div fx-cer justify-end gap-8px col-span-2>
+    <div fx-cer justify-end gap-8px mt-20px>
       <ab-button type="warn" @click="getConfig">Cancel</ab-button>
       <ab-button @click="setConfig">Apply</ab-button>
     </div>
