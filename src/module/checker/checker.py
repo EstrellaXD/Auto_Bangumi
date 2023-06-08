39c7699711a18c1a0fbb8c286ad9e5cc83c11278
EstrellaXD <estrellaxd05@gmail.com>
2023-06-08 12:28:20 +0800

    feat: 优化启动逻辑

--- src/module/checker/checker.py
+++ src/module/checker/checker.py
@@ -47,7 +47,8 @@ class Checker:
     @staticmethod
     def check_first_run() -> bool:
         token_exist = False if settings.rss_parser.token in ["", "token"] else True
-        if token_exist:
-            return False
-        else:
-            return True
+        if settings.rss_parser.enable:
+            if token_exist:
+                return False
+            else:
+                return True
