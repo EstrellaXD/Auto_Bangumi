0475643e35089e105e79a54f5ab9774ab1defa53
EstrellaXD <estrellaxd05@gmail.com>
2023-07-23 17:21:15 +0800

    docs: Update some docs.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -144,6 +144,10 @@ export default defineConfig({
             text: "常见问题",
             link: "/faq/常见问题",
           },
+          {
+            text: "网络问题",
+            link: "/faq/mikan-network",
+          }
         ],
       },
       {
