079f3d6f9d158c38203061a65932ca8eb4352402
EstrellaXD <estrellaxd05@gmail.com>
2023-08-15 20:27:38 +0800

    feat: add new search list api.

--- backend/src/module/api/search.py
+++ backend/src/module/api/search.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Query, Depends
 from fastapi.responses import StreamingResponse
 
-from module.searcher import SearchTorrent
+from module.searcher import SearchTorrent, SEARCH_CONFIG
 from module.security.api import get_current_user, UNAUTHORIZED
 from module.models import Torrent
 
@@ -25,3 +25,10 @@ async def search_torrents(
             content=st.analyse_keyword(keywords=keywords, site=site),
             media_type="application/json",
         )
+
+
+@router.get("/provider", response_model=list[str])
+async def search_provider(current_user=Depends(get_current_user)):
+    if not current_user:
+        raise UNAUTHORIZED
+    return list(SEARCH_CONFIG.keys())
