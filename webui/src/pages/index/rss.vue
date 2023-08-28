02eaafc4fa27cb6fa2a2e25ce15bc97563cb2891
EstrellaXD <estrellaxd05@gmail.com>
2023-08-28 18:24:28 +0800

    webui: rss i18n update.

--- webui/src/pages/index/rss.vue
+++ webui/src/pages/index/rss.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-const { rss } = storeToRefs(useRSSStore());
-const { getAll, updateRSS, deleteRSS } = useRSSStore();
+const {rss} = storeToRefs(useRSSStore());
+const {getAll, updateRSS, deleteRSS} = useRSSStore();
 
 onActivated(() => {
   getAll();
@@ -11,21 +11,24 @@ definePage({
 </script>
 
 <template>
-  <div>
-    <ab-rss-item
-        v-for="i in rss"
-        :key="i.id"
-        :name="i.name"
-        :url="i.url"
-        :enable="i.enable"
-        :parser="i.parser"
-        :aggregate="i.aggregate">
-    </ab-rss-item>
-  </div>
-  <div>
-    <ab-button-group flex>
-      <ab-button icon="edit">{{ $t('config.apply') }}</ab-button>
-      <ab-button class="type-warn" text="delete">{{ $t('config.apply') }}</ab-button>
-    </ab-button-group>
-  </div>
+  <ab-fold-panel :title="$t('rss.title')">
+    <div>
+      <ab-rss-item
+          v-for="i in rss"
+          :key="i.id"
+          :name="i.name"
+          :url="i.url"
+          :enable="i.enable"
+          :parser="i.parser"
+          :aggregate="i.aggregate">
+      </ab-rss-item>
+    </div>
+    <div v-show="!open" line my-12px></div>
+    <div>
+      <ab-button-group flex>
+        <ab-button icon="edit">{{ $t('rss.delete') }}</ab-button>
+        <ab-button class="type-warn" text="delete">{{ $t('rss.disable') }}</ab-button>
+      </ab-button-group>
+    </div>
+  </ab-fold-panel>
 </template>
