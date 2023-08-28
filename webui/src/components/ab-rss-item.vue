89fc6693fb4a9d821dc3c84473e8f935b03a8ef7
EstrellaXD <estrellaxd05@gmail.com>
2023-08-28 18:24:50 +0800

    webui: rss class update

---
+++ webui/src/components/ab-rss-item.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+
+withDefaults(
+    defineProps<{
+      name: string;
+      url: string;
+      enable: boolean;
+      aggregate: boolean;
+      parser: string;
+    }>(),
+    {
+      enable: false,
+    }
+);
+</script>
+
+<template>
+    <div flex>
+      <ab-checkbox
+          small
+          :model-value="enable"
+      />
+      <div text-h3 truncate>{{ name }}</div>
+      <div w-300px text-h3 truncate>{{ url }}</div>
+      <div>{{ parser }}</div>
+    </div>
+</template>
---
+++ webui/src/store/rss.ts
@@ -0,0 +1,49 @@
+import type { RSS } from '#/rss';
+import type { ApiSuccess } from '#/api';
+
+export const useRSSStore = defineStore('rss', () => {
+    const message = useMessage();
+    const rss = ref<RSS[]>();
+
+    const { execute: getAll, onResult: onRSSResult } = useApi(
+        apiRSS.get
+    );
+    const { execute: updateRSS, onResult: onUpdateRSSResult } = useApi(
+        apiRSS.update
+    );
+    const { execute: deleteRSS, onResult: onDeleteRSSResult } = useApi(
+        apiRSS.delete
+    );
+
+
+    onRSSResult((res) => {
+        function sort(arr: RSS[]) {
+            return arr.sort((a, b) => b.id - a.id);
+        }
+
+        const enabled = sort(res.filter((e) => e.enabled));
+        const disabled = sort(res.filter((e) => !e.enabled));
+
+        rss.value = [...enabled, ...disabled];
+    });
+
+    function refresh() {
+        getAll();
+    }
+
+    function actionSuccess(apiRes: ApiSuccess) {
+        message.success(apiRes.msg_en);
+        refresh();
+    }
+
+    onUpdateRSSResult(actionSuccess);
+    onDeleteRSSResult(actionSuccess);
+
+    return {
+        rss,
+        getAll,
+        refresh,
+        updateRSS,
+        deleteRSS,
+    };
+});
