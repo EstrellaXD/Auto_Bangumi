de5bf3fd614b479f68766c2bb9669ca7b9dcdc29
EstrellaXD <estrellaxd05@gmail.com>
2023-08-30 01:24:27 +0800

    fix: unexpected version update.

--- backend/src/module/update/version_check.py
+++ backend/src/module/update/version_check.py
@@ -3,9 +3,10 @@ from module.conf import VERSION
 
 def version_check() -> bool:
     with open("config/version.txt", "rw") as f:
-        version = f.read()
-        if VERSION > version:
-            f.write(VERSION)
+        # Read last version
+        versions = f.readlines()
+        if VERSION[:3] > versions[-1][:3]:
+            f.write(VERSION[:3] + "\n")
             return False
         else:
             return True
