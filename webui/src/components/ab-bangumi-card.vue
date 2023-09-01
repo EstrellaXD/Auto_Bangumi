f204bcd6b6f2bb8019a4663930105153b879f18d
EstrellaXD <estrellaxd05@gmail.com>
2023-09-01 17:10:02 +0800

    webui: add story in bangumi card.

--- webui/src/components/ab-bangumi-card.vue
+++ webui/src/components/ab-bangumi-card.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
-import { ErrorPicture, Write } from '@icon-park/vue-next';
+import {ErrorPicture, Write} from '@icon-park/vue-next';
 
 withDefaults(
-  defineProps<{
-    poster: string;
-    name: string;
-    season: number;
-  }>(),
-  {}
+    defineProps<{
+      poster: string;
+      name: string;
+      season: number;
+      group: string;
+    }>(),
+    {}
 );
 
 defineEmits(['click']);
@@ -18,48 +19,55 @@ defineEmits(['click']);
     <div rounded-4px overflow-hidden poster-shandow rel>
       <div w-full h-210px>
         <template v-if="poster !== ''">
-          <img :src="`https://mikanani.me${poster}`" alt="poster" wh-full />
+          <img :src="`https://mikanani.me${poster}`" alt="poster" wh-full/>
         </template>
 
         <template v-else>
           <div wh-full f-cer border="1 white">
-            <ErrorPicture theme="outline" size="24" fill="#333" />
+            <ErrorPicture theme="outline" size="24" fill="#333"/>
           </div>
         </template>
       </div>
 
       <div
-        abs
-        f-cer
-        z-1
-        inset-0
-        opacity-0
-        transition-all
-        duration-300
-        hover:backdrop-blur-2px
-        hover:bg-white
-        hover:bg-opacity-30
-        hover:opacity-100
-        active:duration-0
-        active:bg-opacity-60
-        class="group"
+          abs
+          f-cer
+          z-1
+          inset-0
+          opacity-0
+          transition-all
+          duration-300
+          hover:backdrop-blur-2px
+          hover:bg-white
+          hover:bg-opacity-30
+          hover:opacity-100
+          active:duration-0
+          active:bg-opacity-60
+          class="group"
       >
         <div
-          text-white
-          rounded="1/2"
-          wh-44px
-          f-cer
-          bg-theme-row
-          class="group-active:poster-pen-active"
+            text-white
+            rounded="1/2"
+            wh-44px
+            f-cer
+            bg-theme-row
+            class="group-active:poster-pen-active"
         >
-          <Write size="20" />
+          <Write size="20"/>
         </div>
       </div>
     </div>
 
-    <div px-4px py-8px>
+    <div px-4px py-4px>
       <div text-h3 truncate>{{ name }}</div>
-      <div text-main>Season {{ season }}</div>
+      <div flex space-x-4px>
+        <ab-tag
+          :title="`Season ${season}`"
+          />
+        <ab-tag
+          :title="group"
+          />
+      </div>
     </div>
   </div>
 </template>
