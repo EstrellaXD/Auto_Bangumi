a54c1066fcda1b66839875be8d7f3efde0c9f199
EstrellaXD <estrellaxd05@gmail.com>
2023-09-23 20:51:41 +0800

    fix: update logic of version update judgment.

--- backend/src/module/update/version_check.py
+++ backend/src/module/update/version_check.py
@@ -1,9 +1,11 @@
-
+import semver
 
 from module.conf import VERSION, VERSION_PATH
 
 
 def version_check() -> bool:
+    if VERSION == "DEV_VERSION":
+        return True
     if not VERSION_PATH.exists():
         with open(VERSION_PATH, "w") as f:
             f.write(VERSION + "\n")
@@ -13,11 +15,13 @@ def version_check() -> bool:
             # Read last version
             versions = f.readlines()
             last_version = versions[-1]
-            if VERSION == last_version:
+            last_ver = semver.VersionInfo.parse(last_version)
+            now_ver = semver.VersionInfo.parse(VERSION)
+            if now_ver.minor == last_ver.minor:
                 return True
             else:
-                if VERSION[:3] > versions[-1][:3]:
-                    f.write(VERSION[:3] + "\n")
+                if now_ver.minor > last_ver.minor:
+                    f.write(VERSION + "\n")
                     return False
                 else:
                     return True
