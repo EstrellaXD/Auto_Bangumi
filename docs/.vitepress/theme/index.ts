d8c0cf11976132927ee820aa1398c32b2236eb09
EstrellaXD <estrellaxd05@gmail.com>
2023-09-27 23:51:33 +0800

    docs: fix build error.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,45 +1,50 @@
 // https://vitepress.dev/guide/custom-theme
-import { 
-  h, 
-  onMounted, 
-  watch,
-  nextTick,
+import {
+    h,
+    onMounted,
+    watch,
+    nextTick,
 } from 'vue'
 import Theme from 'vitepress/theme'
-import { useRoute } from 'vitepress'
+import {useRoute} from 'vitepress'
 import mediumZoom from 'medium-zoom'
+import Documate from '@documate/vue'
+import '@documate/vue/dist/style.css'
 import HomePreviewWebUI from './components/HomePreviewWebUI.vue'
 
 import './style.css'
 
 export default {
-  extends: Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'home-features-after': () => h(HomePreviewWebUI),
-    })
-  },
-  setup() {
-    const route = useRoute()
-    const initZoom = () => {
-      /**
-       * Allow images to be zoomed in on click 
-       * https://github.com/vuejs/vitepress/issues/854
-       */
-      mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' })
-    }
-    
-    onMounted(() => {
-      initZoom()
-    })
+    extends: Theme,
+    Layout: () => {
+        return h(Theme.Layout, null, {
+            // https://vitepress.dev/guide/extending-default-theme#layout-slots
+            'home-features-after': () => h(HomePreviewWebUI),
+            'nav-bar-content-before': () => h(Documate, {
+                endpoint: '',
+            }),
+        })
+    },
+    setup() {
+        const route = useRoute()
+        const initZoom = () => {
+            /**
+             * Allow images to be zoomed in on click
+             * https://github.com/vuejs/vitepress/issues/854
+             */
+            mediumZoom('[data-zoomable]', {background: 'var(--vp-c-bg)'})
+        }
 
-    watch(
-      () => route.path,
-      () => nextTick(initZoom),
-    )
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+        onMounted(() => {
+            initZoom()
+        })
+
+        watch(
+            () => route.path,
+            () => nextTick(initZoom),
+        )
+    },
+    enhanceApp({app, router, siteData}) {
+        // ...
+    }
 }
