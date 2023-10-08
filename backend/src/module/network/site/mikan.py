6523b01042219273492cdc725bc778b11804279c
jue0115 <46835138+jue0115@users.noreply.github.com>
2023-10-08 18:30:21 +0800

    fix nyaa search error

--- backend/src/module/network/site/mikan.py
+++ backend/src/module/network/site/mikan.py
@@ -2,10 +2,16 @@ def rss_parser(soup):
     torrent_titles = []
     torrent_urls = []
     torrent_homepage = []
-    for item in soup.findall("./channel/item"):
-        torrent_titles.append(item.find("title").text)
-        torrent_urls.append(item.find("enclosure").attrib["url"])
-        torrent_homepage.append(item.find("link").text)
+    if not len(soup.findall("./channel/item/enclosure")):
+        for item in soup.findall("./channel/item"):
+            torrent_titles.append(item.find("title").text)
+            torrent_urls.append(item.find("link").text)
+            torrent_homepage.append(item.find("guid").text)
+    else:
+        for item in soup.findall("./channel/item"):
+            torrent_titles.append(item.find("title").text)
+            torrent_urls.append(item.find("enclosure").attrib["url"])
+            torrent_homepage.append(item.find("link").text)
     return torrent_titles, torrent_urls, torrent_homepage
 
 
