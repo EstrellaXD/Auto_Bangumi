07efbac16d207b706a582c219959c8a46160301c
EstrellaXD <estrellaxd05@gmail.com>
2023-10-10 14:45:42 +0800

    feat: replace requests with httpx.

--- backend/src/module/network/request_contents.py
+++ backend/src/module/network/request_contents.py
@@ -12,14 +12,14 @@ logger = logging.getLogger(__name__)
 
 
 class RequestContent(RequestURL):
-    def get_torrents(
+    async def get_torrents(
         self,
         _url: str,
         _filter: str = "|".join(settings.rss_parser.filter),
         limit: int = None,
         retry: int = 3,
     ) -> list[Torrent]:
-        soup = self.get_xml(_url, retry)
+        soup = await self.get_xml(_url, retry)
         if soup:
             torrent_titles, torrent_urls, torrent_homepage = rss_parser(soup)
             torrents: list[Torrent] = []
@@ -38,38 +38,40 @@ class RequestContent(RequestURL):
             logger.warning(f"[Network] Failed to get torrents: {_url}")
             return []
 
-    def get_xml(self, _url, retry: int = 3) -> xml.etree.ElementTree.Element:
-        req = self.get_url(_url, retry)
+    async def get_xml(self, _url, retry: int = 3) -> xml.etree.ElementTree.Element:
+        req = await self.get_url(_url, retry)
         if req:
             return xml.etree.ElementTree.fromstring(req.text)
 
     # API JSON
-    def get_json(self, _url) -> dict:
-        req = self.get_url(_url)
+    async def get_json(self, _url) -> dict:
+        req = await self.get_url(_url)
         if req:
             return req.json()
 
-    def post_json(self, _url, data: dict) -> dict:
-        return self.post_url(_url, data).json()
+    async def post_json(self, _url, data: dict) -> dict:
+        return await self.post_url(_url, data)
 
-    def post_data(self, _url, data: dict) -> dict:
-        return self.post_url(_url, data)
+    async def post_data(self, _url, data: dict) -> dict:
+        return await self.post_json(_url, data)
 
-    def post_files(self, _url, data: dict, files: dict) -> dict:
-        return self.post_form(_url, data, files)
+    async def post_files(self, _url, data: dict, files: dict) -> dict:
+        return await self.post_form(_url, data, files)
 
-    def get_html(self, _url):
-        return self.get_url(_url).text
+    async def get_html(self, _url):
+        req = await self.get_url(_url)
+        if req:
+            return req.text
 
-    def get_content(self, _url):
-        req = self.get_url(_url)
+    async def get_content(self, _url):
+        req = await self.get_url(_url)
         if req:
             return req.content
 
-    def check_connection(self, _url):
-        return self.check_url(_url)
+    async def check_connection(self, _url):
+        return await self.check_url(_url)
 
-    def get_rss_title(self, _url):
-        soup = self.get_xml(_url)
+    async def get_rss_title(self, _url):
+        soup = await self.get_xml(_url)
         if soup:
             return soup.find("./channel/title").text
