0abf9eb102147e5a21d56f7e1f876a01910350e5
Usagi no Niku <chenbohan911@163.com>
2023-12-25 21:33:48 +0800

    fix: fix test cases

--- backend/src/test/test_title_parser.py
+++ backend/src/test/test_title_parser.py
@@ -6,7 +6,7 @@ from module.parser.title_parser import TitleParser
 class TestTitleParser:
     def test_parse_without_openai(self):
         text = "[梦蓝字幕组]New Doraemon 哆啦A梦新番[747][2023.02.25][AVC][1080P][GB_JP][MP4]"
-        result = TitleParser.raw_parser(text)
+        result, _ = TitleParser.raw_parser(text)
         assert result.group_name == "梦蓝字幕组"
         assert result.title_raw == "New Doraemon"
         assert result.dpi == "1080P"
@@ -19,7 +19,7 @@ class TestTitleParser:
     )
     def test_parse_with_openai(self):
         text = "[梦蓝字幕组]New Doraemon 哆啦A梦新番[747][2023.02.25][AVC][1080P][GB_JP][MP4]"
-        result = TitleParser.raw_parser(text)
+        result, _ = TitleParser.raw_parser(text)
         assert result.group_name == "梦蓝字幕组"
         assert result.title_raw == "New Doraemon"
         assert result.dpi == "1080P"
