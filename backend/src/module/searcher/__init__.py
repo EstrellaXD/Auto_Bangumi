5bb39c133f62dc446dfc5bac289d64c4a0264a3d
東雲柊 <gtx2shino@gmail.com>
2024-08-03 16:01:49 +0800

    Asynchronous Searcher

--- backend/src/module/searcher/__init__.py
+++ backend/src/module/searcher/__init__.py
@@ -1,2 +1,5 @@
-from .provider import SEARCH_CONFIG
+from module.conf import SEARCH_CONFIG
+
 from .searcher import SearchTorrent
+
+__all__ = ["SEARCH_CONFIG", "SearchTorrent",]
--- backend/src/module/searcher/searcher.py
+++ backend/src/module/searcher/searcher.py
@@ -1,11 +1,11 @@
+import asyncio
 import json
 from typing import TypeAlias
 
 from module.models import Bangumi, RSSItem, Torrent
 from module.network import RequestContent
 from module.rss import RSSAnalyser
-
-from .provider import search_url
+from module.searcher.provider import search_url
 
 SEARCH_KEY = [
     "group_name",
@@ -19,37 +19,61 @@ SEARCH_KEY = [
 BangumiJSON: TypeAlias = str
 
 
-class SearchTorrent(RequestContent, RSSAnalyser):
-    def search_torrents(self, rss_item: RSSItem) -> list[Torrent]:
-        return self.get_torrents(rss_item.url)
+class SearchTorrent:
+    def __init__(self) -> None:
+        self.req = RequestContent
+
+    async def search_torrents(self, rss_item: RSSItem) -> list[Torrent]:
+        async with self.req() as req:
+            return await req.get_torrents(rss_item.url)
         # torrents = self.get_torrents(rss_item.url)
         # return torrents
 
-    def analyse_keyword(
+    async def analyse_keyword(
         self, keywords: list[str], site: str = "mikan", limit: int = 5
-    ) -> BangumiJSON:
+    ) -> list[BangumiJSON]:
         rss_item = search_url(site, keywords)
-        torrents = self.search_torrents(rss_item)
+        torrents = await self.search_torrents(rss_item)
         # yield for EventSourceResponse (Server Send)
         exist_list = []
-        for torrent in torrents:
-            if len(exist_list) >= limit:
-                break
-            bangumi = self.torrent_to_data(torrent=torrent, rss=rss_item)
+        tasks = []
+        bangumi_list = []
+        for torrent in torrents[:20]:
+            tasks.append(RSSAnalyser().torrent_to_data(torrent=torrent, rss=rss_item))
+        bangumis = await asyncio.gather(*tasks, return_exceptions=True)
+        for bangumi in bangumis:
             if bangumi:
                 special_link = self.special_url(bangumi, site).url
                 if special_link not in exist_list:
+                    exist_list.append(special_link)
                     bangumi.rss_link = special_link
                     exist_list.append(special_link)
-                    yield json.dumps(bangumi.dict(), separators=(",", ":"))
+                    bangumi_list.append(
+                        json.dumps(bangumi.dict(), separators=(",", ":"))
+                    )
+                    if len(exist_list) == 5:
+                        break
+        return iter(bangumi_list)
 
     @staticmethod
     def special_url(data: Bangumi, site: str) -> RSSItem:
         keywords = [getattr(data, key) for key in SEARCH_KEY if getattr(data, key)]
+        print(f"{keywords=}")
         url = search_url(site, keywords)
         return url
 
-    def search_season(self, data: Bangumi, site: str = "mikan") -> list[Torrent]:
+    async def search_season(self, data: Bangumi, site: str = "mikan") -> list[Torrent]:
         rss_item = self.special_url(data, site)
-        torrents = self.search_torrents(rss_item)
+        torrents = await self.search_torrents(rss_item)
         return [torrent for torrent in torrents if data.title_raw in torrent.name]
+
+
+if __name__ == "__main__":
+    test = Bangumi(
+        official_title="【我推的孩子】",
+        title_raw="Oshi No Ko",
+        group_name="ANi",
+        rss_link="https://mikanani.me/RSS/Bangumi?bangumiId=3407&subgroupid=583",
+    )
+    st = SearchTorrent()
+    print(asyncio.run(st.search_season(test)))
