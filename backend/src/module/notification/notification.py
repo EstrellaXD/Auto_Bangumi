85d506db4555af9b9e9e0914f362eb1f8a65e36e
admin <1065576641@qq.com>
2024-08-15 20:23:57 +0800

    新增gofity推送模式

--- backend/src/module/notification/notification.py
+++ backend/src/module/notification/notification.py
@@ -9,6 +9,7 @@ from .plugin import (
     ServerChanNotification,
     TelegramNotification,
     WecomNotification,
+    GotifyNotification,
 )
 
 logger = logging.getLogger(__name__)
@@ -23,6 +24,8 @@ def getClient(type: str):
         return BarkNotification
     elif type.lower() == "wecom":
         return WecomNotification
+    elif type.lower() == "gotify":
+        return GotifyNotification
     else:
         return None
 
