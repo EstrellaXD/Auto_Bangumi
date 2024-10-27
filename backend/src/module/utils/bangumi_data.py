7e5621820c05f4bc7b119f35f81a3123d3e72152
東雲柊 <gtx2shino@gmail.com>
2024-10-27 09:57:43 +0800

    fix dmhy hash

--- backend/src/module/utils/bangumi_data.py
+++ backend/src/module/utils/bangumi_data.py
@@ -8,7 +8,7 @@ def get_hash(torrent_url: str) -> str | None:
     hash_pattern_dict = {
         "magnet_hash_pattern": re.compile(r"urn:btih:([a-fA-F0-9]{40})"),
         "torrent_hash_pattern": re.compile(r"/([a-fA-F0-9]{7,40})\.torrent"),
-        "dmhy_hash_pattern": re.compile(r"([A-Z0-9]{32})"),
+        "dmhy_hash_pattern": re.compile(r"urn:btih:([A-Z0-9]{32})"),
     }
     for hash_pattern in hash_pattern_dict.values():
         ans = re.search(hash_pattern, torrent_url)
