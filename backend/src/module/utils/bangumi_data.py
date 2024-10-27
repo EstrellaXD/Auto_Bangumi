d287cf54c04d2599d79947f0627ec7b8e630d603
東雲柊 <gtx2shino@gmail.com>
2024-10-27 09:52:03 +0800

    fix dmhy torrent hash

--- backend/src/module/utils/bangumi_data.py
+++ backend/src/module/utils/bangumi_data.py
@@ -7,10 +7,16 @@ logger = logging.getLogger(__name__)
 def get_hash(torrent_url: str) -> str | None:
     hash_pattern_dict = {
         "magnet_hash_pattern": re.compile(r"urn:btih:([a-fA-F0-9]{40})"),
-        "torrent_hash_pattern": re.compile(r"/([a-fA-F0-9]{40})\.torrent"),
-        "nyaa_hash_pattern": re.compile(r"([0-9]+).torrent"),
+        "torrent_hash_pattern": re.compile(r"/([a-fA-F0-9]{7,40})\.torrent"),
+        "dmhy_hash_pattern": re.compile(r"([A-Z0-9]{32})"),
     }
     for hash_pattern in hash_pattern_dict.values():
         ans = re.search(hash_pattern, torrent_url)
         if ans:
             return ans[1]
+
+
+if __name__ == "__main__":
+    test_url = r"https://nyaa.land/download/1864806.torrent"
+    test_url = "magnet:?xt=urn:btih:R5NMZKCR2GYMZJRYO6AUGPKZPTQPAFDM"
+    print(get_hash(test_url))
