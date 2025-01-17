545b670338b47842d4c5f68f633d9d2113aa5242
liming <827182486@qq.com>
2025-01-17 14:57:58 +0800

    优化代码

--- backend/src/module/rss/engine.py
+++ backend/src/module/rss/engine.py
@@ -118,14 +118,16 @@ class RSSEngine(Database):
         for rss_item in rss_items:
             new_torrents = self.pull_rss(rss_item)
             # Get all enabled bangumi data
+            add_torrents = []
             for torrent in new_torrents:
                 matched_data = self.match_torrent(torrent)
                 if matched_data:
                     if client.add_torrent(torrent, matched_data):
                         logger.debug(f"[Engine] Add torrent {torrent.name} to client")
-                    torrent.downloaded = True
+                        torrent.downloaded = True
+                        add_torrents.append(torrent)
             # Add all torrents to database
-            self.torrent.add_all(new_torrents)
+            self.torrent.add_all(add_torrents)
 
     def download_bangumi(self, bangumi: Bangumi):
         with RequestContent() as req:
