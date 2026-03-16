7ddb0e1b19d4d2272e1ff74204e93a622dffcbab
azril <hollowloong@gmail.com>
2026-03-16 13:26:07 +0000

    add backproxy support

--- webui/src/utils/poster.ts
+++ webui/src/utils/poster.ts
@@ -1,5 +1,9 @@
 export function resolvePosterUrl(link: string | null | undefined): string {
   if (!link) return '';
-  if (link.startsWith('http://') || link.startsWith('https://')) return link;
-  return `/${link}`;
+
+  if (link.startsWith('http://') || link.startsWith('https://')) {
+    return link;
+  }
+
+  return link.replace(/^\//, '');
 }
